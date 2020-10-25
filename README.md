fe4c5c31e1e13fabc4a9615b0510d08e0456b140
dbeccaria826 <beccdaniel@gmail.com>
2020-10-25 15:57:52 -0400

    Setup: added all basic dependencies/files to get up and running

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # github-jobs-api-react
+
+## Purpose
+
+Build a frontend user interface that pulls in job data from the github api.
+
+## Stack
+
+React
+Github API
+Styled Components/Theme-ing
+
+## Checklist:
+### User Story 1:
+[] Search bar the top of the page
+[] User use should see and be able to interact with a search bar
+[] User should see search results displayed beneath search bar in an easy to read format
+[] User should be able to click on any one search result and see more information displayed about the job listing
+
+### Optional:
+[] User can save/mark job postings they are interested in for later viewing
+
+### Component Tree
+
+Searchbar (Header) --> Card Components --> Links --> Job Posting
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(svg|png|jpg|gif|pdf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'imgs',
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+};
